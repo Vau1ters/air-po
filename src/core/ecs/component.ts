3a8bb18f81108c0b9976987b646d9250f40774d6
Poly <0122Haruo@gmail.com>
2020-02-22 19:51:42 +0900

    create player component

--- src/core/ecs/component.ts
+++ src/core/ecs/component.ts
@@ -1,11 +1,13 @@
 import { PositionComponent } from '../components/positionComponent'
 import { VelocityComponent } from '../components/velocityComponent'
 import { DrawComponent } from '../components/drawComponent'
+import { PlayerComponent } from '../components/playerComponent'
 
 export interface ComponentMap {
   Position: PositionComponent
   Velocity: VelocityComponent
   Draw: DrawComponent
+  Player: PlayerComponent
 }
 
 export type ComponentName = keyof ComponentMap
