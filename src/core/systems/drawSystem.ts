a2bc1e2c99581301b830eee5cf55c7712c9a7288
Poly <0122Haruo@gmail.com>
2020-02-22 17:02:36 +0900

    create draw system

---
+++ src/core/systems/drawSystem.ts
@@ -0,0 +1,43 @@
+import { System } from '../ecs/system'
+import { Family, FamilyBuilder } from '../ecs/family'
+import { World } from '../ecs/world'
+import { DrawComponent } from '../components/drawComponent'
+import { PositionComponent } from '../components/positionComponent'
+import { Container } from 'pixi.js'
+import { Entity } from '../ecs/entity'
+
+export default class DrawSystem extends System {
+  private family: Family
+
+  private container: Container = new Container()
+
+  public constructor(world: World, stage: Container) {
+    super(world)
+
+    stage.addChild(this.container)
+
+    this.family = new FamilyBuilder(world).include('Draw').build()
+
+    for (const entity of this.family.entities) {
+      this.onContainerAdded(entity)
+    }
+    this.family.entityAddedEvent.addObserver(entity =>
+      this.onContainerAdded(entity)
+    )
+  }
+
+  public onContainerAdded(entity: Entity): void {
+    const container = entity.getComponent('Draw') as DrawComponent
+    this.container.addChild(container)
+  }
+
+  public update(): void {
+    for (const entity of this.family.entities) {
+      const container = entity.getComponent('Draw') as DrawComponent
+      if (entity.hasComponent('Position')) {
+        const position = entity.getComponent('Position') as PositionComponent
+        container.position.set(position.x, position.y)
+      }
+    }
+  }
+}
