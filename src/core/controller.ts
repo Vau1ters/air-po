f6750304771a8af3df781b7b8445574ded1b0b3a
Poly <0122Haruo@gmail.com>
2020-02-22 19:50:15 +0900

    fix controller

--- src/core/controller.ts
+++ src/core/controller.ts
@@ -4,13 +4,23 @@ export class KeyController {
   private static readonly keyPressedMap: Map<KeyCode, boolean> = new Map()
 
   private static pressKey(keyCode: KeyCode): void {
+    if (!this.keyPressingMap.get(keyCode)) {
+      this.keyPressedMap.set(keyCode, true)
+    }
     this.keyPressingMap.set(keyCode, true)
-    this.keyPressedMap.set(keyCode, true)
   }
 
   private static releaseKey(keyCode: KeyCode): void {
     this.keyPressingMap.set(keyCode, false)
     this.keyPressedMap.set(keyCode, false)
+  }
+
+  private static parseString(s: string): string {
+    let k = s
+    if (k.length === 1) k = k.toUpperCase()
+    if (k === ' ') k = 'Space'
+    k = k.replace(/Arrow/, '')
+    return k
   }
 
   private static isKeyCode(s: string): s is KeyCode {
@@ -54,16 +64,24 @@ export class KeyController {
     )
   }
 
+  private static stringToKeyCode(s: string): KeyCode | undefined {
+    const k = this.parseString(s)
+    if (this.isKeyCode(k)) {
+      return k
+    }
+    return undefined
+  }
+
   public static init(): void {
     window.addEventListener('keydown', e => {
-      const key = e.key
-      if (this.isKeyCode(key)) {
+      const key = this.stringToKeyCode(e.key)
+      if (key) {
         this.pressKey(key)
       }
     })
     window.addEventListener('keyup', e => {
-      const key = e.key
-      if (this.isKeyCode(key)) {
+      const key = this.stringToKeyCode(e.key)
+      if (key) {
         this.releaseKey(key)
       }
     })
@@ -79,8 +97,8 @@ export class KeyController {
 
   // 毎フレーム呼び出す
   public static onUpdateFinished(): void {
-    for (const keyCode of this.keyPressingMap.keys()) {
-      this.keyPressingMap.set(keyCode, false)
+    for (const keyCode of this.keyPressedMap.keys()) {
+      this.keyPressedMap.set(keyCode, false)
     }
   }
 }
