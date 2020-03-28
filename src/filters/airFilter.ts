06225ea6963bac2d83cb44dad10ca40630500c04
Poly <0122Haruo@gmail.com>
2020-03-28 11:56:47 +0900

    add filter for air rendering

--- src/filters/airFilter.ts
+++ src/filters/airFilter.ts
@@ -18,8 +18,23 @@ export class AirFilter extends Filter {
     })
   }
 
-  public addAir(...airs: AirDefinition[]): void {
-    airs.forEach(air => {
+  public get airs(): Array<AirDefinition> {
+    const airs = []
+    for (let i = 0; i < this.uniforms.points.length / 3; i++) {
+      airs.push({
+        center: {
+          x: this.uniforms.points[i * 3],
+          y: this.uniforms.points[i * 3 + 1],
+        },
+        radius: this.uniforms.points[i * 3 + 2],
+      })
+    }
+    return airs
+  }
+
+  public set airs(airs: Array<AirDefinition>) {
+    this.uniforms.points = []
+    airs.map(air => {
       this.uniforms.points.push(air.center.x)
       this.uniforms.points.push(air.center.y)
       this.uniforms.points.push(air.radius)
