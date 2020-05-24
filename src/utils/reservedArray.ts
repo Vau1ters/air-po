7c1f04c8158f9f0d0d73a0a76e3eb46bef753101
sobaya <sobaya007@gmail.com>
2020-05-24 18:16:44 +0900

    Add: public on each member of reservedArray

--- src/utils/reservedArray.ts
+++ src/utils/reservedArray.ts
@@ -2,27 +2,27 @@ export class ReservedArray<T> {
   private a: Array<T>
   private len: number
 
-  constructor(cap: number) {
+  public constructor(cap: number) {
     this.a = new Array<T>(cap)
     this.len = 0
   }
 
-  at(idx: number): T {
+  public at(idx: number): T {
     return this.a[idx]
   }
 
-  get length(): number {
+  public get length(): number {
     return this.len
   }
 
-  push(e: T): void {
+  public push(e: T): void {
     this.a[this.len++] = e
     if (this.len === this.a.length) {
       this.a = this.a.concat(this.a)
     }
   }
 
-  toArray(): T[] {
+  public toArray(): T[] {
     const result = new Array<T>(this.length)
     for (let i = 0; i < this.length; i++) {
       result[i] = this.at(i)
