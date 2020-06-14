fcdd5ca0bec3dab7eed8b65a5ba8fab8d52c9c79
Poly <0122Haruo@gmail.com>
2020-06-14 14:54:50 +0900

    Create AirHolderSystem

--- src/core/systems/playerControlSystem.ts
+++ src/core/systems/playerControlSystem.ts
@@ -5,13 +5,11 @@ import { World } from '../ecs/world'
 import { KeyController } from '../controller'
 import { PlayerComponent } from '../components/playerComponent'
 import { RigidBodyComponent } from '../components/rigidBodyComponent'
-import { Collider, AirCollider } from '../components/colliderComponent'
+import { Collider } from '../components/colliderComponent'
 import { HorizontalDirectionComponent } from '../components/directionComponent'
 import { BulletFactory } from '../entities/bulletFactory'
 import { assert } from '../../utils/assertion'
 import { AirHolderComponent } from '../components/airHolderComponent'
-import { AirComponent } from '../components/airComponent'
-import { PositionComponent } from '../components/positionComponent'
 
 export class PlayerControlSystem extends System {
   private family: Family
@@ -32,9 +30,6 @@ export class PlayerControlSystem extends System {
       for (const c of collider.colliders) {
         if (c.tag.has('playerFoot')) {
           c.callbacks.add(PlayerControlSystem.footCollisionCallback)
-        }
-        if (c.tag.has('playerBody')) {
-          c.callbacks.add(PlayerControlSystem.bodySensor)
         }
       }
     }
@@ -80,7 +75,7 @@ export class PlayerControlSystem extends System {
       // air consume
       const airHolder = entity.getComponent('AirHolder')
       assert(airHolder instanceof AirHolderComponent)
-      airHolder.consume(player.status.air.consumeSpeed)
+      airHolder.consume()
     }
 
     KeyController.onUpdateFinished()
@@ -108,42 +103,6 @@ export class PlayerControlSystem extends System {
     if (!other.isSensor) {
       const pc = player.component.entity.getComponent('Player')
       if (pc) pc.landing = true
-    }
-  }
-
-  private static bodySensor(playerCollider: Collider, otherCollider: Collider): void {
-    // collect air
-    if (otherCollider.tag.has('air')) {
-      assert(otherCollider instanceof AirCollider)
-
-      const player = playerCollider.component.entity.getComponent('Player') as PlayerComponent
-      const position = playerCollider.component.entity.getComponent('Position') as PositionComponent
-      const airHolder = playerCollider.component.entity.getComponent(
-        'AirHolder'
-      ) as AirHolderComponent
-
-      const hitAirs: Entity[] = otherCollider.airFamily.entityArray.filter(
-        (a: Entity) => (a.getComponent('Air') as AirComponent).hit
-      )
-      const nearestAir = hitAirs.reduce((a, b) => {
-        const aa = a.getComponent('Air') as AirComponent
-        const pa = a.getComponent('Position') as PositionComponent
-        const ab = b.getComponent('Air') as AirComponent
-        const pb = b.getComponent('Position') as PositionComponent
-        if (pa.sub(position).lengthSq() / aa.quantity < pb.sub(position).lengthSq() / ab.quantity) {
-          return a
-        } else {
-          return b
-        }
-      })
-      const air = nearestAir.getComponent('Air') as AirComponent
-      const collectSpeed = Math.min(
-        player.status.air.collectSpeed,
-        airHolder.maxQuantity - airHolder.currentQuantity,
-        air.quantity
-      )
-      airHolder.collect(collectSpeed)
-      air.decrease(collectSpeed)
     }
   }
 }
