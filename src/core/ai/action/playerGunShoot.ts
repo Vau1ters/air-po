c270b4da2c84979cc91e3d9d33d2f676388186c5
uynet <highsate@gmail.com>
2020-09-13 16:53:41 +0900

    soundtest

--- src/core/ai/action/playerGunShoot.ts
+++ src/core/ai/action/playerGunShoot.ts
@@ -4,6 +4,7 @@ import { Entity } from '../../ecs/entity'
 import { MouseController } from '../../systems/controlSystem'
 import { BulletFactory } from '../../entities/bulletFactory'
 import { application, windowSize } from '../../application'
+import PIXI from 'pixi-sound'
 
 const SETTING = {
   CONSUME_SPEED: 10,
@@ -30,6 +31,9 @@ export const playerGunShoot = function*(entity: Entity, world: World): Behaviour
     if (airHolder.currentQuantity >= SETTING.CONSUME_SPEED) {
       airHolder.consumeBy(SETTING.CONSUME_SPEED)
 
+      const path = '../../../../res/sound/shot.wav'
+      const sound = PIXI.Sound.from(path)
+      sound.play()
       // 弾を打つ
       bulletFactory.player = entity
       const player = entity.getComponent('Player')
