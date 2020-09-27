7f62e2b8b3d68152c4bed977655eb835702e3cac
uynet <highsate@gmail.com>
2020-09-27 14:15:02 +0900

    asser

--- src/core/sound/sound.ts
+++ src/core/sound/sound.ts
@@ -1,11 +1,12 @@
 import shot from '../../../res/sound/shot.wav'
 import snibee from '../../../res/sound/snibee.wav'
 import PIXI from 'pixi-sound'
+import { assert } from '../../utils/assertion'
 
 export const soundStore: { [key: string]: PIXI.Sound } = {}
 export const play = (name: string): void => {
   const sound = soundStore[name]
-  console.assert(sound !== undefined)
+  assert(sound !== undefined)
   sound.play()
 }
 export const init = (): void => {
