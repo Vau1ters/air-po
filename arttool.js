4ea47071720dbb3cbdacfc0e66a072b44506071c
uynet <highsate@gmail.com>
2020-11-22 19:51:25 +0900

    必要なimport文を生成する関数

--- arttool.js
+++ arttool.js
@@ -1,10 +1,23 @@
 const fs = require("fs");
 
+function genImportLine(file) {
+  const filename = file.split(".")[0];
+  const ext = file.split(".")[1];
+  const prefix = ext == "png" ? "Img" : "Setting";
+
+  const objname = filename + prefix;
+  const dirname = ext == "png" ? "@res/image/" : "@res/map/";
+
+  return "import " + objname + " from " + dirname + file;
+}
+
 fs.readdir("./res/image", (err, imgs) => {
   if (!imgs) {
     console.log(err);
   }
-  imgs.forEach((img) => {});
+  imgs.forEach((img) => {
+    console.log(genImportLine(img));
+  });
 });
 
 const a = fs.readFileSync("./src/core/graphics/art.ts", { encoding: "utf-8" });
