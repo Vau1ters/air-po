9514fac4b70a0758056349b3106cc57961a896dc
Poly <0122Haruo@gmail.com>
2020-12-20 16:33:12 +0900

    Improve slime1 motion

--- src/game/ai/entity/slime1/slime1AI.ts
+++ src/game/ai/entity/slime1/slime1AI.ts
@@ -4,17 +4,29 @@ import { Behaviour } from '@core/behaviour/behaviour'
 import { suspendable } from '@core/behaviour/suspendable'
 import { isAlive } from '../common/condition/isAlive'
 import { move, Direction } from '../common/action/move'
-import { animate, animateLoop } from '../common/action/animate'
+import { animate } from '../common/action/animate'
 import { kill } from '../common/action/kill'
 import { emitAir } from '../common/action/emitAir'
 import { parallelAny } from '@core/behaviour/composite'
 
 const slime1Move = function*(entity: Entity): Behaviour<void> {
   while (true) {
-    for (let i = 0; i < 3; i++) yield* animate(entity, 'Idle', 0.1)
-    yield* parallelAny([animateLoop(entity, 'Walk', 0.15), move(entity, Direction.Right, 0.3, 200)])
-    for (let i = 0; i < 3; i++) yield* animate(entity, 'Idle', 0.1)
-    yield* parallelAny([animateLoop(entity, 'Walk', 0.15), move(entity, Direction.Left, 0.3, 200)])
+    for (let i = 0; i < 3; i++) yield* animate(entity, 'Idling', 0.12)
+    for (let i = 0; i < 5; i++) {
+      yield* parallelAny([
+        animate(entity, 'Jumping', 0.2),
+        move(entity, Direction.Right, 0.5, Infinity),
+      ])
+      yield* animate(entity, 'Landing', 0.2)
+    }
+    for (let i = 0; i < 3; i++) yield* animate(entity, 'Idling', 0.12)
+    for (let i = 0; i < 5; i++) {
+      yield* parallelAny([
+        animate(entity, 'Jumping', 0.2),
+        move(entity, Direction.Left, 0.5, Infinity),
+      ])
+      yield* animate(entity, 'Landing', 0.2)
+    }
   }
 }
 
