595d8afbd7e0cec4dea9113b75830c7469dc7e84
Poly <0122Haruo@gmail.com>
2021-02-14 19:04:31 +0900

    Refactoring fukidashiAI

--- src/game/ai/entity/fukidashi/fukidashiAI.ts
+++ src/game/ai/entity/fukidashi/fukidashiAI.ts
@@ -1,6 +1,9 @@
 import { windowSize } from '@core/application'
 import { Behaviour } from '@core/behaviour/behaviour'
 import { parallelAll } from '@core/behaviour/composite'
+import { ease } from '@core/behaviour/easing/easing'
+import { In, Out } from '@core/behaviour/easing/functions'
+import { wait } from '@core/behaviour/wait'
 import { Entity } from '@core/ecs/entity'
 import { Vec2 } from '@core/math/vec2'
 import { BitmapText, Sprite } from 'pixi.js'
@@ -29,39 +32,37 @@ const chase = function*(fukidashi: Entity, target: Entity, camera: Entity): Beha
   }
 }
 
+const updateBitmapText = (bitmapText: BitmapText, newText: string): void => {
+  bitmapText.text = newText
+  bitmapText.updateText()
+}
+
 const animate = function*(fukidashi: Entity): Behaviour<void> {
   const draw = fukidashi.getComponent('UI')
-  const text = draw.getChildByName('text') as BitmapText
+  const bitmapText = draw.getChildByName('text') as BitmapText
   const [filter] = draw.filters
 
-  yield
+  const wholeText = bitmapText.text
+  updateBitmapText(bitmapText, '')
 
-  const wholeText = text.text
-  const updateText = (newText: string): void => {
-    text.text = newText
-    text.updateText()
-  }
-  updateText('')
+  const popInFukidashiScale = ease(Out.quad)(20, value => (filter.uniforms.scale = value))
+  const popInFukidashiAngle = ease(t => Math.sin(t * 24) * Math.exp(-t * t * 10) * 0.1)(
+    80,
+    value => (filter.uniforms.angle = value)
+  )
+  yield* parallelAll([popInFukidashiScale, popInFukidashiAngle])
 
-  let t = 0
-  while (t < 1) {
-    t += 0.01
-    const t2 = Math.min(1, t * 5)
-    filter.uniforms.scale = t2 * (2 - t2)
-    filter.uniforms.angle = Math.sin(t * 30) * Math.exp(-t * t * 10) * 0.1
-    yield
+  while (bitmapText.text.length < wholeText.length) {
+    yield* wait(4)
+    updateBitmapText(bitmapText, wholeText.substr(0, bitmapText.text.length + 1))
   }
-  while (text.text.length < wholeText.length) {
-    for (let i = 0; i < 3; i++) yield
-    updateText(wholeText.substr(0, text.text.length + 1))
-    yield
-  }
-  while (0 < t) {
-    t -= 0.01
-    const t2 = Math.min(1, t * 5)
-    filter.uniforms.scale = t2 * (2 - t2)
-    yield
-  }
+  yield* wait(100)
+
+  const popOutFukidashiScale = ease(In.quad)(20, value => (filter.uniforms.scale = value), {
+    from: 1,
+    to: 0,
+  })
+  yield* popOutFukidashiScale
 }
 
 export const fukidashiAI = function(
