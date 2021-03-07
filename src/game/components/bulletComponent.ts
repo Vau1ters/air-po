e332b662c170c8ae943aa53c295256911b6aec82
sobaya <sobaya007@gmail.com>
2021-03-07 17:29:00 +0900

    Fix: BulletComponent

--- src/game/components/bulletComponent.ts
+++ src/game/components/bulletComponent.ts
@@ -1,7 +1,5 @@
 export class BulletComponent {
   private static readonly INITIAL_LIFE = 20
 
-  constructor(public life: number = BulletComponent.INITIAL_LIFE) {
-    this.life = life
-  }
+  constructor(public life: number = BulletComponent.INITIAL_LIFE) {}
 }
