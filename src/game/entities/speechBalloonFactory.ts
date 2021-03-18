0fd8199e7df21a8bba6d169c02b09997ba4fca06
sobaya <sobaya007@gmail.com>
2021-03-18 15:07:33 +0900

    Add: hiragana/katanaka auto conversion

--- src/game/entities/speechBalloonFactory.ts
+++ src/game/entities/speechBalloonFactory.ts
@@ -46,7 +46,7 @@ export class SpeechBalloonFactory extends EntityFactory {
     ]
     ui.sortableChildren = true
 
-    const text = new BitmapText(this.text, {
+    const text = new BitmapText(this.normalize(this.text), {
       fontName: 'got',
       fontSize: 8,
     })
@@ -69,5 +69,70 @@ export class SpeechBalloonFactory extends EntityFactory {
       new PositionComponent(windowSize.width * 0.5, sprite.height * 0.5)
     )
     return entity
+  }
+
+  private normalize(text: string): string {
+    const table: { [key: string]: string } = {
+      が: 'か゛',
+      ぎ: 'き゛',
+      ぐ: 'く゛',
+      げ: 'け゛',
+      ご: 'こ゛',
+      ざ: 'さ゛',
+      じ: 'し゛',
+      ず: 'す゛',
+      ぜ: 'せ゛',
+      ぞ: 'そ゛',
+      だ: 'た゛',
+      ぢ: 'ち゛',
+      づ: 'つ゛',
+      で: 'て゛',
+      ど: 'と゛',
+      ば: 'は゛',
+      び: 'ひ゛',
+      ぶ: 'ふ゛',
+      べ: 'へ゛',
+      ぼ: 'ほ゛',
+      ぱ: 'は゜',
+      ぴ: 'ひ゜',
+      ぷ: 'ふ゜',
+      ぺ: 'へ゜',
+      ぽ: 'ほ゜',
+      ガ: 'カ゛',
+      ギ: 'キ゛',
+      グ: 'ク゛',
+      ゲ: 'ケ゛',
+      ゴ: 'コ゛',
+      ザ: 'サ゛',
+      ジ: 'シ゛',
+      ズ: 'ズ゛',
+      ゼ: 'ゼ゛',
+      ゾ: 'ゾ゛',
+      ダ: 'タ゛',
+      ヂ: 'チ゛',
+      ヅ: 'ツ゛',
+      デ: 'テ゛',
+      ド: 'ト゛',
+      バ: 'ハ゛',
+      ビ: 'ヒ゛',
+      ブ: 'フ゛',
+      ベ: 'ヘ゛',
+      ボ: 'ホ゛',
+      パ: 'ハ゜',
+      ピ: 'ヒ゜',
+      プ: 'フ゜',
+      ペ: 'ヘ゜',
+      ポ: 'ホ゜',
+    }
+    let result = ''
+    for (let i = 0; i < text.length; i++) {
+      const c = text.charAt(i)
+      if (c in table) {
+        result += table[c]
+      } else {
+        result += c
+      }
+    }
+    return result
   }
 }
