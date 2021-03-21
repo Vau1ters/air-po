3981f31eedc9447675e522e04ae266e97058ec3b
Poly <0122Haruo@gmail.com>
2021-03-21 14:48:25 +0900

    Fix bug that player can jet without air

--- src/game/ai/entity/player/playerJet.ts
+++ src/game/ai/entity/player/playerJet.ts
@@ -5,11 +5,13 @@ import { Vec2 } from '@core/math/vec2'
 import { World } from '@core/ecs/world'
 import { JetEffectFactory } from '@game/entities/jetEffectFactory'
 import GravitySystem from '@game/systems/gravitySystem'
+import { wait } from '@core/behaviour/wait'
 
 const SETTING = {
   CONSUME_SPEED: 0.1,
   JET_SPEED: 140,
   JET_POWER: 600,
+  JET_COOL_TIME: 10,
 }
 const calcPlayerAngle = (): Vec2 => {
   const angle = new Vec2()
@@ -30,19 +32,24 @@ const calcPlayerAngle = (): Vec2 => {
   return angle.normalize()
 }
 
+const jetCondition = (entity: Entity): boolean => {
+  const airHolder = entity.getComponent('AirHolder')
+  return KeyController.isActionPressing('Jet') && airHolder.quantity >= SETTING.CONSUME_SPEED
+}
+
 export const playerJet = function*(entity: Entity, world: World): Behaviour<void> {
   const body = entity.getComponent('RigidBody')
   const airHolder = entity.getComponent('AirHolder')
   const animState = entity.getComponent('AnimationState')
 
   while (true) {
-    while (!KeyController.isActionPressing('Jet')) yield
+    while (!jetCondition(entity)) yield
 
     if (body.velocity.length() > SETTING.JET_SPEED) {
       body.velocity.assign(body.velocity.normalize().mul(SETTING.JET_SPEED))
     }
 
-    while (KeyController.isActionPressing('Jet')) {
+    while (jetCondition(entity)) {
       const playerAngle = calcPlayerAngle()
       const velocity = body.velocity
       const acceleration = body.acceleration
@@ -63,11 +70,7 @@ export const playerJet = function*(entity: Entity, world: World): Behaviour<void
         world.addEntity(jetEffect)
       }
 
-      if (
-        playerAngle.lengthSq() > 0 &&
-        airHolder.quantity >= SETTING.CONSUME_SPEED &&
-        velocity.length() < SETTING.JET_SPEED
-      ) {
+      if (playerAngle.lengthSq() > 0 && velocity.length() < SETTING.JET_SPEED) {
         acceleration.x += playerAngle.x * SETTING.JET_POWER
         acceleration.y += playerAngle.y * SETTING.JET_POWER
       }
@@ -75,6 +78,6 @@ export const playerJet = function*(entity: Entity, world: World): Behaviour<void
       yield
     }
 
-    yield
+    yield* wait(SETTING.JET_COOL_TIME)
   }
 }
