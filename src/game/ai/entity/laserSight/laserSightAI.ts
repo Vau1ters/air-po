6758241da6b1e8d166757ee1bdd1231d5d3aa2f2
Poly <0122Haruo@gmail.com>
2021-04-13 13:02:06 +0900

    Refactoring

--- src/game/ai/entity/laserSight/laserSightAI.ts
+++ src/game/ai/entity/laserSight/laserSightAI.ts
@@ -1,6 +1,8 @@
 import { windowSize } from '@core/application'
 import { Behaviour } from '@core/behaviour/behaviour'
 import { parallelAll } from '@core/behaviour/composite'
+import { ease } from '@core/behaviour/easing/easing'
+import { In, Out } from '@core/behaviour/easing/functions'
 import { CollisionResultRayAABB } from '@core/collision/collision/Ray_AABB'
 import { Ray } from '@core/collision/geometry/ray'
 import { Entity } from '@core/ecs/entity'
@@ -11,11 +13,38 @@ import { Vec2 } from '@core/math/vec2'
 import { CollisionCallbackArgs } from '@game/components/colliderComponent'
 import { LaserSightLockFactory } from '@game/entities/laserSightLockFactory'
 import { MouseController } from '@game/systems/controlSystem'
+import { assert } from '@utils/assertion'
 import { Graphics } from 'pixi.js'
 
 type HitResult = {
   point: Vec2
   entity?: Entity
+}
+type Lock = { lock: Entity; despawn: () => void }
+type LaserSightState =
+  | {
+      state: 'Locking'
+      target: Entity
+      chasing: number
+    }
+  | {
+      state: 'Free'
+      point: Vec2
+    }
+
+const updateInvisibleRay = function*(laser: Entity, world: World): Behaviour<void> {
+  const playerFamily = new FamilyBuilder(world).include('Player').build()
+  const [collider] = laser.getComponent('Collider').colliders
+  while (true) {
+    const [player] = playerFamily.entityArray
+    const mousePosition = MouseController.position
+    const ray = collider.geometry as Ray
+    const position = player.getComponent('Position')
+
+    ray.origin = position
+    ray.direction = mousePosition.sub(new Vec2(windowSize.width / 2, windowSize.height / 2))
+    yield
+  }
 }
 
 const getClosestHitGenerator = function*(
@@ -45,50 +74,140 @@ const getClosestHitGenerator = function*(
     }
   }
 }
-
-type State = { lock: Lock | undefined }
-
-type Lock = { lock: Entity; target: Entity; despawn: () => void }
-
-const updateInvisibleRay = function*(laser: Entity, world: World): Behaviour<void> {
-  const playerFamily = new FamilyBuilder(world).include('Player').build()
-  const [collider] = laser.getComponent('Collider').colliders
-  while (true) {
-    const [player] = playerFamily.entityArray
-    const mousePosition = MouseController.position
-    const ray = collider.geometry as Ray
-    const position = player.getComponent('Position')
-
-    ray.origin = position
-    ray.direction = mousePosition.sub(new Vec2(windowSize.width / 2, windowSize.height / 2))
-    yield
+const isDistantEnough = (ray: Ray, entity: Entity): boolean => {
+  return ray.distance(entity.getComponent('Position')) > 20
+}
+const shouldLockEntity = (entity: Entity, ray: Ray): boolean => {
+  return (
+    entity.hasComponent('HP') &&
+    entity.getComponent('HP').hp > 0 &&
+    entity
+      .getComponent('Position')
+      .sub(ray.origin)
+      .length() < 160 && // 離れすぎてたらロックしない
+    !MouseController.isMousePressing('Right')
+  )
+}
+const spawnLock = (target: Entity, world: World): Lock => {
+  let despawning = false
+  const lock = new LaserSightLockFactory(target, () => despawning, world).create()
+  world.addEntity(lock)
+  return {
+    lock,
+    despawn: (): void => {
+      despawning = true
+    },
   }
 }
 
-const updateVisibleRay = function*(laser: Entity, world: World, state: State): Behaviour<void> {
+const getLaserSightStateGenerator = function*(
+  player: Entity,
+  laser: Entity,
+  world: World
+): Generator<LaserSightState, void> {
+  const getClosestHit = getClosestHitGenerator(player, laser)
+  const [collider] = laser.getComponent('Collider').colliders
+  const ray = collider.geometry as Ray
+
+  const freeAimGenerator = function*(): Generator<LaserSightState, Entity> {
+    while (true) {
+      const { value } = getClosestHit.next()
+      assert(value instanceof Object, 'Unexpected error')
+
+      const { entity, point } = value
+
+      if (entity && shouldLockEntity(entity, ray)) return entity
+
+      yield {
+        state: 'Free',
+        point,
+      }
+    }
+  }
+
+  const lockingAimGenerator = function*(entity: Entity): Generator<void> {
+    while (true) {
+      const { value } = getClosestHit.next()
+      assert(value instanceof Object, 'Unexpected error')
+
+      const { entity: currentHittingEntity } = value
+      if (
+        currentHittingEntity &&
+        currentHittingEntity.id !== entity.id && // 当たっているEntityが変わっていなければロックし続ける
+        (isDistantEnough(ray, entity) || !shouldLockEntity(entity, ray))
+      ) {
+        return
+      }
+
+      yield
+    }
+  }
+
+  while (true) {
+    const entity = yield* freeAimGenerator()
+
+    const lock = spawnLock(entity, world)
+    const state: LaserSightState = {
+      state: 'Locking',
+      target: entity,
+      chasing: 0,
+    }
+    const easeOutChase = ease(Out.quad)(
+      3,
+      value => {
+        state.chasing = value
+      },
+      {
+        from: 0,
+        to: 1,
+      }
+    )
+    const easeInChase = ease(In.quad)(
+      3,
+      value => {
+        state.chasing = value
+      },
+      {
+        from: 1,
+        to: 0,
+      }
+    )
+
+    for (const _ of easeOutChase) {
+      yield state
+    }
+    for (const _ of lockingAimGenerator(entity)) {
+      yield state
+    }
+    for (const _ of easeInChase) {
+      yield state
+    }
+
+    lock.despawn()
+  }
+}
+
+const updateVisibleRay = function*(laser: Entity, world: World): Behaviour<void> {
   const playerFamily = new FamilyBuilder(world).include('Player').build()
   const [g] = laser.getComponent('Draw').children as [Graphics]
-  const targetPos = new Vec2()
-  let chasing = 0
-  while (true) {
-    const [player] = playerFamily.entityArray
+  const [player] = playerFamily.entityArray
+  const getLaserSightState = getLaserSightStateGenerator(player, laser, world)
+
+  for (const state of getLaserSightState) {
     const mousePosition = MouseController.position
     const start = player.getComponent('Position')
 
-    if (state.lock) {
-      targetPos.assign(state.lock.target.getComponent('Position'))
-    }
-
-    const end0 = start.add(mousePosition.sub(new Vec2(windowSize.width / 2, windowSize.height / 2)))
-    const end1 = targetPos
-
-    if (state.lock) {
-      chasing = Math.min(chasing + 0.3, 1)
+    const end = new Vec2()
+    if (state.state === 'Free') {
+      end.assign(state.point)
     } else {
-      chasing = Math.max(chasing - 0.3, 0)
-    }
+      const end0 = start.add(
+        mousePosition.sub(new Vec2(windowSize.width / 2, windowSize.height / 2))
+      )
+      const end1 = state.target.getComponent('Position')
 
-    const end = end0.add(end1.sub(end0).mul(chasing))
+      end.assign(end0.add(end1.sub(end0).mul(state.chasing)))
+    }
 
     player.getComponent('Player').targetPosition = end
 
@@ -98,66 +217,11 @@ const updateVisibleRay = function*(laser: Entity, world: World, state: State): B
     g.lineTo(end.x, end.y)
     g.beginFill(0xff0000)
     g.drawCircle(end.x, end.y, 2)
-    yield
-  }
-}
 
-const updateLock = function*(
-  player: Entity,
-  laser: Entity,
-  world: World,
-  state: State
-): Behaviour<void> {
-  const getClosestHit = getClosestHitGenerator(player, laser)
-  const [collider] = laser.getComponent('Collider').colliders
-  const ray = collider.geometry as Ray
-
-  const isDistantEnough = (ray: Ray, entity: Entity): boolean => {
-    return ray.distance(entity.getComponent('Position')) > 20
-  }
-
-  const spawnLock = (target: Entity): Lock => {
-    let despawning = false
-    const lock = new LaserSightLockFactory(target, () => despawning, world).create()
-    world.addEntity(lock)
-    return {
-      lock,
-      target,
-      despawn: (): void => {
-        despawning = true
-      },
-    }
-  }
-
-  for (const { entity } of getClosestHit) {
-    if (state.lock) {
-      if (
-        isDistantEnough(ray, state.lock.target) ||
-        state.lock.target.getComponent('HP').hp <= 0 ||
-        MouseController.isMousePressing('Right')
-      ) {
-        state.lock.despawn()
-        state.lock = undefined
-      }
-    } else {
-      if (
-        entity &&
-        entity.hasComponent('HP') &&
-        entity.getComponent('HP').hp > 0 &&
-        !MouseController.isMousePressing('Right')
-      ) {
-        state.lock = spawnLock(entity)
-      }
-    }
     yield
   }
 }
 
 export const laserSightAI = (player: Entity, laser: Entity, world: World): Behaviour<void> => {
-  const state: State = { lock: undefined }
-  return parallelAll([
-    updateVisibleRay(laser, world, state),
-    updateInvisibleRay(laser, world),
-    updateLock(player, laser, world, state),
-  ])
+  return parallelAll([updateInvisibleRay(laser, world), updateVisibleRay(laser, world)])
 }
