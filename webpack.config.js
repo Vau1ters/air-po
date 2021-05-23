4ac3ef7538516491d3dab2923b3e1e2b2596fbe0
sobaya <sobaya007@gmail.com>
2021-05-23 14:15:06 +0900

    Fix: upgrade webpack version to 5

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,11 @@ module.exports = {
         configFile: 'tsconfig.json',
       }),
     ],
+    fallback: {
+      buffer: require.resolve("buffer/"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify")
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
