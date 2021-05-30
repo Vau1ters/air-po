1b1c981a4d16f92d8afd2c7e475390632b75e009
Poly <0122Haruo@gmail.com>
2021-05-30 16:35:35 +0900

    Add background2

--- src/core/graphics/art.ts
+++ src/core/graphics/art.ts
@@ -7,6 +7,8 @@ import airGeyserSetting from '@res/setting/airGeyser.json'
 import airGeyserImg from '@res/image/airGeyser.png'
 import background1Setting from '@res/setting/background1.json'
 import background1Img from '@res/image/background1.png'
+import background2Setting from '@res/setting/background2.json'
+import background2Img from '@res/image/background2.png'
 import ballBulletSetting from '@res/setting/ballBullet.json'
 import ballBulletImg from '@res/image/ballBullet.png'
 import balloonVineSetting from '@res/setting/balloonVine.json'
@@ -103,6 +105,7 @@ export const init = async (): Promise<void> => {
   // LOAD_RESOURCE
   textureStore.airGeyser = await buildTextureCache(airGeyserImg, airGeyserSetting)
   textureStore.background1 = await buildTextureCache(background1Img, background1Setting)
+  textureStore.background2 = await buildTextureCache(background2Img, background2Setting)
   textureStore.ballBullet = await buildTextureCache(ballBulletImg, ballBulletSetting)
   textureStore.balloonVine = await buildTextureCache(balloonVineImg, balloonVineSetting)
   textureStore.dandelion = await buildTextureCache(dandelionImg, dandelionSetting)
--- src/game/ai/world/game/gameWorldAI.ts
+++ src/game/ai/world/game/gameWorldAI.ts
@@ -14,6 +14,7 @@ import { Entity } from '@core/ecs/entity'
 import { Vec2 } from '@core/math/vec2'
 import { DrawComponent } from '@game/components/drawComponent'
 import background1Definition from '@res/setting/background1.json'
+import background2Definition from '@res/setting/background2.json'
 import { parseAnimation } from '@core/graphics/animationParser'
 
 const game = function*(world: World): Behaviour<void> {
@@ -32,18 +33,31 @@ const game = function*(world: World): Behaviour<void> {
 
 export const gameWorldAI = (map: Map) =>
   function*(world: World): Behaviour<void> {
-    const bg = new Entity()
-    bg.addComponent('Background', { scrollSpeed: new Vec2(0.9, 0.9) })
-    bg.addComponent('Position', new Vec2())
-    const drawComponent = new DrawComponent({
-      entity: bg,
+    const bg1 = new Entity()
+    bg1.addComponent('Background', { scrollSpeed: new Vec2(0.9, 0.9) })
+    bg1.addComponent('Position', new Vec2())
+    const drawComponent1 = new DrawComponent({
+      entity: bg1,
       child: {
         sprite: parseAnimation(background1Definition),
       },
     })
-    drawComponent.zIndex = -Infinity
-    bg.addComponent('Draw', drawComponent)
-    world.addEntity(bg)
+    drawComponent1.zIndex = -Infinity
+    bg1.addComponent('Draw', drawComponent1)
+    world.addEntity(bg1)
+
+    const bg2 = new Entity()
+    bg2.addComponent('Background', { scrollSpeed: new Vec2(0.3, 0.2) })
+    bg2.addComponent('Position', new Vec2())
+    const drawComponent2 = new DrawComponent({
+      entity: bg2,
+      child: {
+        sprite: parseAnimation(background2Definition),
+      },
+    })
+    drawComponent2.zIndex = -Infinity
+    bg2.addComponent('Draw', drawComponent2)
+    world.addEntity(bg2)
 
     yield* FadeIn(world)
 
