7c2c91961140b73de582736182cf8e3969166602
wake-wakame <ufun-ahan@gmail.com>
2022-02-27 13:28:20 +0900

    オレ...ハタ...オロス...

--- src/game/ai/entity/respawn/respawnAI.ts
+++ src/game/ai/entity/respawn/respawnAI.ts
@@ -1,9 +1,13 @@
 import { Behaviour } from '@core/behaviour/behaviour'
 import { branch, BranchController } from '@core/behaviour/branch'
+import { parallelAny } from '@core/behaviour/composite'
 import { wait } from '@core/behaviour/wait'
 import { Entity } from '@core/ecs/entity'
 import { loadData, StoryStatus, saveData } from '@game/playdata/playdata'
+import { EventNotifier } from '@utils/eventNotifier'
 import { animate } from '../common/action/animate'
+
+const someoneActivated = new EventNotifier<void>()
 
 export const respawnAI = function* (entity: Entity): Behaviour<void> {
   const [collider] = entity.getComponent('Collider').colliders
@@ -11,44 +15,69 @@ export const respawnAI = function* (entity: Entity): Behaviour<void> {
 
   yield* branch({
     Waiting: function* (controller: BranchController<void>): Behaviour<void> {
-      const collisionResults = yield* wait.collision(collider)
-      const [
-        {
-          other: { entity: player },
-        },
-      ] = collisionResults
-      const playerData = player.getComponent('Player').playerData
+      while (true) {
+        const collisionResults = yield* wait.collision(collider)
+        const [
+          {
+            other: { entity: player },
+          },
+        ] = collisionResults
+        const playerData = player.getComponent('Player').playerData
 
-      const currentData = loadData()
-      currentData.storyStatus = StoryStatus.Stage
-      currentData.spawnPoint = entity.getComponent('StagePoint').stagePoint
-      currentData.playerData = playerData
-      currentData.playerData.hp = currentData.playerData.maxHp
-      currentData.playerData.air = player.getComponent('AirHolder').maxQuantity
+        const currentData = loadData()
+        currentData.storyStatus = StoryStatus.Stage
+        currentData.spawnPoint = entity.getComponent('StagePoint').stagePoint
+        currentData.playerData = playerData
+        currentData.playerData.hp = currentData.playerData.maxHp
+        currentData.playerData.air = player.getComponent('AirHolder').maxQuantity
 
-      saveData(currentData)
+        saveData(currentData)
 
-      controller.transit('Activating')
-      yield
+        controller.transit('Activating')
+        yield
+      }
     },
     Activating: function* (controller: BranchController<void>): Behaviour<void> {
-      sound.addSound('flag')
-      yield* animate({
-        entity,
-        state: 'Activating',
-        loopCount: 1,
-      })
+      while (true) {
+        sound.addSound('flag')
+        yield* animate({
+          entity,
+          state: 'Activating',
+          loopCount: 1,
+        })
 
-      controller.transit('Activated')
-      yield
+        controller.transit('Activated')
+        yield
+      }
     },
     Activated: function* (controller: BranchController<void>): Behaviour<void> {
-      yield* animate({
-        entity,
-        state: 'Activated',
-        loopCount: Infinity,
-      })
-      controller.finish()
+      while (true) {
+        someoneActivated.notify()
+
+        yield* parallelAny([
+          animate({
+            entity,
+            state: 'Activated',
+            loopCount: Infinity,
+          }),
+          wait.notification(someoneActivated),
+        ])
+
+        controller.transit('Deactivating')
+        yield
+      }
+    },
+    Deactivating: function* (controller: BranchController<void>): Behaviour<void> {
+      while (true) {
+        yield* animate({
+          entity,
+          state: 'Deactivated',
+          loopCount: 1,
+        })
+
+        controller.transit('Waiting')
+        yield
+      }
     },
   }).start('Waiting')
 }
