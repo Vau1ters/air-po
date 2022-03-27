a96d44917f1795c9b144324b3a0bbf0a8306fab1
Poly <0122Haruo@gmail.com>
2022-03-27 17:59:12 +0900

    WIP

--- src/game/stage/stageList.autogen.ts
+++ src/game/stage/stageList.autogen.ts
@@ -4,12 +4,18 @@
 
 import root from '@res/stage/autogen/root.autogen.json'
 import stage1map1 from '@res/stage/autogen/stage1map1.autogen.json'
+import stage1map1_test from '@res/stage/autogen/stage1map1_test.autogen.json'
+import stage1map2_test from '@res/stage/autogen/stage1map2_test.autogen.json'
+import stage1map3_test from '@res/stage/autogen/stage1map3_test.autogen.json'
 import teststage from '@res/stage/autogen/teststage.autogen.json'
 import teststage2 from '@res/stage/autogen/teststage2.autogen.json'
 
 export const stageList = {
   root,
   stage1map1,
+  stage1map1_test,
+  stage1map2_test,
+  stage1map3_test,
   teststage,
   teststage2,
 }
