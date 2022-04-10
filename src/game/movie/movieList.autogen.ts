fc30737e981ba304bdbcced9449e8ac617259479
Poly <0122Haruo@gmail.com>
2022-04-10 16:33:29 +0900

    Add ShopKeeperFirstContact movie

--- src/game/movie/movieList.autogen.ts
+++ src/game/movie/movieList.autogen.ts
@@ -2,8 +2,10 @@
 // this file is automatically written by script.
 // you can update this file by type "yarn metabuild" command.
 
+import shopKeeperFirstContact from '@res/movie/shopKeeperFirstContact.json'
 import testMovie from '@res/movie/testMovie.json'
 
 export const movieList = {
+  shopKeeperFirstContact,
   testMovie,
 }
