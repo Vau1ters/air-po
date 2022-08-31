5ee3e200dd60c7ce713d6edc9bdd5be91d18201c
Vau1ters <go10.2ka3@gmail.com>
2022-09-01 00:19:43 +0900

    銃チュートリアル

--- src/game/movie/movieList.autogen.ts
+++ src/game/movie/movieList.autogen.ts
@@ -6,6 +6,7 @@ import stage1map1findAirTankMovie from '@res/movie/stage1map1findAirTankMovie.js
 import stage1map1getAirTankMovie from '@res/movie/stage1map1getAirTankMovie.json'
 import stage1map1introMovie from '@res/movie/stage1map1introMovie.json'
 import stage1map1openingMovie from '@res/movie/stage1map1openingMovie.json'
+import stage1map2gunTutorial from '@res/movie/stage1map2gunTutorial.json'
 import testMovie from '@res/movie/testMovie.json'
 
 export const movieList = {
@@ -13,5 +14,6 @@ export const movieList = {
   stage1map1getAirTankMovie,
   stage1map1introMovie,
   stage1map1openingMovie,
+  stage1map2gunTutorial,
   testMovie,
 }
